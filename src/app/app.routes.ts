0996e03bf163fd919615a1f5c3590cf858546525
Saharmizrahi1 <sahar3224@gmail.com>
2024-12-20 10:11:46 +0200

    changes in project srtucture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './features/home-page/home-page.component';
-import { ManageFlightsComponent } from './features/manage-flights/manage-flights.component';
-import { ManageDestinationsComponent } from './features/manage-destinations/manage-destinations.component';
-import  { BookFlightComponent } from './features/book-flight/book-flight.component';
-import { BookSingleFlightComponent } from './features/book-single-flight/book-single-flight.component';
-import { MyBookingsComponent } from './features/my-bookings/my-bookings.component';
+import { ManageFlightsComponent } from './features/flights/pages/manage-flights/manage-flights.component';
+import { ManageDestinationsComponent } from './features/destinations/pages/manage-destinations/manage-destinations.component';
+import  { BookFlightComponent } from './features/bookings/pages/book-flight/book-flight.component';
+import { BookSingleFlightComponent } from './features/bookings/pages/book-single-flight/book-single-flight.component';
+import { MyBookingsComponent } from './features/bookings/pages/my-bookings/my-bookings.component';
 import { HelpComponent } from './features/help/help.component';
-import { SingleFlightComponent } from './features/single-flight/single-flight.component';
-import { SingleDestinationComponent } from './features/single-destination/single-destination.component';
-import { ViewBookingComponent } from './features/view-booking/view-booking.component';
-import { FlightCardComponent } from './features/flight-card/flight-card.component';
+import { SingleFlightComponent } from './features/flights/pages/single-flight/single-flight.component';
+import { SingleDestinationComponent } from './features/destinations/pages/single-destination/single-destination.component';
+import { ViewBookingComponent } from './features/bookings/pages/view-booking/view-booking.component';
+import { FlightCardComponent } from './features/flights/pages/flight-card/flight-card.component';
 
 export const routes: Routes = [
   { path: 'home-page', component: HomePageComponent },
