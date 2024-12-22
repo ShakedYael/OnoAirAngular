aab9bc50ae0503784772270bcbc1397f61e93228
SaharM <sahar3224@gmail.com>
2024-12-22 09:19:47 +0200

    footer added

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  constructor(private router: Router) {}
+
+  goToHelp() {
+    this.router.navigate(['/help']);
+  }
 
 }
