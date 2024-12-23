7bef7af9cb487606cc38cc90361a4a9bb2994008
SaharM <sahar3224@gmail.com>
2024-12-23 16:17:59 +0200

    search a flight

--- src/app/features/flights/pages/search-a-flight/search-a-flight.component.ts
+++ src/app/features/flights/pages/search-a-flight/search-a-flight.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FlightCardComponent } from '../flight-card/flight-card.component';
+
 
 @Component({
-  selector: 'app-search-flight',
-  imports: [],
+  selector: 'app-search-a-flight',
+  imports: [FlightCardComponent],
   templateUrl: './search-a-flight.component.html',
   styleUrl: './search-a-flight.component.css'
 })
