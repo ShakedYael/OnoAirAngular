b8e22cb32f2b190ca8f0b17b5ea53eb3cbef963c
Saharmizrahi1 <sahar3224@gmail.com>
2024-12-25 19:27:54 +0200

    book a flight page = book single flight

--- src/app/features/bookings/pages/book-single-flight/book-single-flight.component.ts
+++ src/app/features/bookings/pages/book-single-flight/book-single-flight.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Flight } from '../../../flights/model/flight.model';
+import { FlightService } from '../../../flights/service/flights.service'; 
+import { CommonModule } from '@angular/common'; 
+
+
 
 @Component({
   selector: 'app-book-single-flight',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-single-flight.component.html',
   styleUrl: './book-single-flight.component.css'
 })
-export class BookSingleFlightComponent {
+export class BookSingleFlightComponent implements OnInit {
+  flight?: Flight;
+
+  constructor(
+    private route: ActivatedRoute,
+    private flightService: FlightService
+  ) {}
+
+  ngOnInit(): void {
+    const flightNo = this.route.snapshot.paramMap.get('flightNo');
+    if (flightNo) {
+      this.flight = this.flightService.get(flightNo);
+    }
+
+
+    /*
+    if (!this.flight) {
+      throw new Error(`Flight with flightNo ${flightNo} not found`);
+    }*/
+    
+  }
+
 
 }
