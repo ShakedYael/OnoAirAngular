a13afaed66fac22bedb11f2d5021ff6497afd6ba
Saharmizrahi1 <sahar3224@gmail.com>
2024-12-25 19:47:15 +0200

    search a flight table  + add this to home page

--- src/app/features/flights/pages/search-a-flight/search-a-flight.component.ts
+++ src/app/features/flights/pages/search-a-flight/search-a-flight.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import { FlightCardComponent } from "../flight-card/flight-card.component";
+import { Router } from '@angular/router';
+import { FlightService } from '../../service/flights.service';
+import { Flight } from '../../model/flight.model';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-search-a-flight',
-  imports: [FlightCardComponent],
+  imports: [FlightCardComponent, CommonModule],
   templateUrl: './search-a-flight.component.html',
   styleUrl: './search-a-flight.component.css'
 })
-export class SearchAFlightComponent {
+export class SearchAFlightComponent  implements OnInit {
+  flights: Flight[] = [];
+
+  constructor(
+    private flightService: FlightService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.flights = this.flightService.list();
+  }
+
+  bookFlight(flightNo: string): void {
+    this.router.navigate(['/book-single-flight', { flightNo }]);
+  }
+
+
+
+
 
 }
--- src/app/features/home-page/home-page.component.ts
+++ src/app/features/home-page/home-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FlightCardComponent } from '../flights/pages/flight-card/flight-card.component';
-
+import { SearchAFlightComponent } from '../flights/pages/search-a-flight/search-a-flight.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ FlightCardComponent],
+  imports: [ FlightCardComponent, SearchAFlightComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
