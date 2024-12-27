81bacbac0db40ae4d571fc0bf563ed12a07215d4
Saharmizrahi1 <sahar3224@gmail.com>
2024-12-27 11:20:39 +0200

    favicon+ view booking

--- src/app/features/bookings/pages/view-booking/view-booking.component.ts
+++ src/app/features/bookings/pages/view-booking/view-booking.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { BookingsService } from '../../service/bookings.service';
+import { ActivatedRoute } from '@angular/router';
+import { Booking } from '../../model/booking.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-booking',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view-booking.component.html',
   styleUrl: './view-booking.component.css'
 })
-export class ViewBookingComponent {
+export class ViewBookingComponent implements OnInit {
+  booking: Booking | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private bookingsService: BookingsService
+  ) {}
+
+  ngOnInit(): void {
+    const bookingId = this.route.snapshot.paramMap.get('id');
+    if (bookingId) {
+      this.booking = this.bookingsService.get(bookingId);
+    }
+  }
 
 }
